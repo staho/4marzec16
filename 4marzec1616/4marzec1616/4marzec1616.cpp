9eb994b3b9a49d7334363dd15f0154bed4c3b463
staho <stacho688@gmail.com>
2016-03-09 18:05:05 +0100

    Add add, display, clean and return/delete last(first in)

--- 4marzec1616/4marzec1616/4marzec1616.cpp
+++ 4marzec1616/4marzec1616/4marzec1616.cpp
@@ -11,8 +11,10 @@ struct node { // struktura listy
 	node * next;
 };
 
-void dodaj(node *&, int &);
-void wyswietl(node *);
+void add(node *&, int &);
+void display(node *);
+void clean(node *&);
+int delete_last(node *&);
 
 int main()
 {
@@ -22,30 +24,44 @@ int main()
 	{
 		system("CLS");
 		cout << "Co chcesz zrobic z lista" << endl
-			<< "1. Dodac element?\t\t\t" << "2. Skasowac element <pierwszy z listy>" << endl
-			<< "3. Wyswietlic pelna liste\t\t5.Wyjsc" << endl;
+			<< "1. Dodac element?\t\t\t" << "2. Skasowac element <ostatni z listy>" << endl
+			<< "3. Wyswietlic pelna liste\t\t4. Posprzatac" << endl << "5.Wyjsc" << endl;
 		cin >> choice;
 		switch (choice)
 		{
 		case 1:
-			dodaj(H, counter);
+			add(H, counter);
 			break;
 		case 2:
+			try
+			{
+				cout << "Sciagniety element to " << delete_last(H) << endl;
+			}
+			catch (int)
+			{
+				cout << "Brak elementow" << endl;
+			}
+			system("PAUSE");
 			break;
 		case 3:
-			wyswietl(H);
+			display(H);
 			break;
+		case 4:
+			delete_last(H);
+			break;
+
 		default:
 			break;
 		}
 
 	}
+	clean(H);
 	delete H;
 	return 0;
 }
 
 /*Sekcja funkcji do maina <te z switcha>*/
-void dodaj(node *&H, int &counter)
+void add(node *&H, int &counter)
 {
 	cout << "Podaj liczbe do wrzucenia" << endl;
 	int x;
@@ -57,7 +73,7 @@ void dodaj(node *&H, int &counter)
 
 	counter++;
 }
-void wyswietl(node *H)
+void display(node *H)
 {
 	while (H != NULL)
 	{
@@ -66,4 +82,36 @@ void wyswietl(node *H)
 	}
 	cout << endl;
 	system("PAUSE");
+}
+void clean(node *&H)
+{
+	while (H != NULL)
+	{
+		node * p = H;
+		H = p->next;
+		delete p;
+	}
+}
+int delete_last(node *&H)
+{
+	node * p = H;
+	int x;
+	if (H == NULL) throw 1;
+	else if (H->next == NULL)
+	{
+		x = H->val;
+		p = H;
+		H = NULL;
+		delete p;
+		return x;
+	}
+	else
+	{
+		while (p->next->next != NULL)
+			p = p->next;
+		x = p->next->val;
+		delete p->next;
+		p->next = NULL;
+		return x;
+	}
 }
